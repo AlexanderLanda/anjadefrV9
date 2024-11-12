743dcd3392cab34cdc676d7dd0b364402d77c4fc
Alexander Landa <alanda@acid.cl>
2024-11-12 01:45:29 +0100

    barra de carga de proceso de creado y limpieza de campos al terminar

--- src/app/visualComponents/crear-noticias/crear-noticias.component.ts
+++ src/app/visualComponents/crear-noticias/crear-noticias.component.ts
@@ -10,6 +10,8 @@ import { NoticiaServiceImpl } from '../../Core/Service/Implements/NoticiaService
 export class CrearNoticiasComponent implements OnInit {
   noticiaForm: FormGroup;
   archivos: File[] = []; // Almacena los archivos seleccionados
+  cargando = false; // Indicador de carga
+  mensajeExito = ''; // Mensaje de éxito
 
   constructor(
     private formBuilder: FormBuilder,
@@ -26,16 +28,22 @@ export class CrearNoticiasComponent implements OnInit {
   }
   onSubmit() {
     if (this.noticiaForm.valid) {
+      this.cargando = true; // Inicia el indicador de carga
+      this.mensajeExito = ''; // Limpia el mensaje de éxito previo
       const noticiaData = this.noticiaForm.value;
       const imagenesArray = noticiaData.imagenesLinks.split(',').map(link => link.trim());
       
       this.noticiaService.crearNoticia({...noticiaData, imagenes: imagenesArray}).subscribe(
         response => {
+          this.cargando = false; // Finaliza el indicador de carga
+          this.mensajeExito = 'Noticia insertada correctamente';
+          this.noticiaForm.reset(); // Limpia el formulario
           console.log('Noticia creada', response);
           // Lógica adicional después de crear la noticia
         },
         error => {
           console.error('Error al crear la noticia', error);
+          this.cargando = false;
         }
       );
     }
